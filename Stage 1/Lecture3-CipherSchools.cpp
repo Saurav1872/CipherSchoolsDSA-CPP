7272a5ace4aeba6e83d58b29f1cf948a4e3638de
Saurav1872 <saurav.suman1807@gmail.com>
2023-10-08 14:15:10 +0530

    Lecture3 Pt-2

--- Stage 1/Lecture3-CipherSchools.cpp
+++ Stage 1/Lecture3-CipherSchools.cpp
@@ -3,4 +3,23 @@ using namespace std;
 
 int main() {
     
+    int a = 3;
+    int b = 5;
+
+    // a = 3;
+    // b = 5;
+
+    // int c;
+
+    // c = a+b;
+
+    // cout << c;
+
+    cout << a+b << endl << "a+b";
+
+    cout << a+b;
+    cout << endl;
+    cout << "a+b";
+
+
 }
